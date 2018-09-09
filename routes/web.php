c3511a9ad846dfadba7d19cf74a160e2afb8cadc
M.Aljehany <moath@moath.co>
2018-09-10 00:54:47 +0300

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::auth();
+
+Route::get('/', 'TodoController@index')->name('todo.index');
+Route::get('/create', 'TodoController@create')->name('todo.create');
+Route::post('/store', 'TodoController@store')->name('todo.store');
+Route::get('/edit/{id}', 'TodoController@edit')->name('todo.edit');
+Route::put('/update/{id}', 'TodoController@update')->name('todo.update');
+Route::delete('/delete/{id}', 'TodoController@destroy')->name('todo.delete');
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
